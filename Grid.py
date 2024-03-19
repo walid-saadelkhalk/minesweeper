8132849b7e326286c76d49b7261fba6c7a0c95f4
Dhjali <barbara.vanni@laplateforme.io>
2024-03-19 10:35:07 +0100

    [ADD] method matrice_size / test : ok

--- Grid.py
+++ Grid.py
@@ -13,18 +13,36 @@ class Grid:
     
     def get_level(self):
         return self.__level
+    
     def set_level(self, level):
         self.__level = level
 
     def get_matrice(self):
         return self.__matrice
+    
     def set_matrice(self, matrice):
         self.__matrice = matrice
     
     def get_list_cells_objects(self):
         return self.__list_cells_objects 
+    
     def set_list_cells_objects(self, list_cells_objects):
         self.__list_cells_objects = list_cells_objects
+
+    
+    def matrice_size(self):
+        if self.__level == 'easy':
+            total_x = 7
+            total_y = 8
+        elif self.__level == 'medium':
+            total_x = 16
+            total_y = 16
+        elif self.__level == 'hard':
+            total_x = 31
+            total_y = 16
+        return total_x, total_y
+    
+    
     
     
     
