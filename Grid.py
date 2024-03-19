84107ac4e58073627db17fe01c65e72cabbed4c8
Dhjali <barbara.vanni@laplateforme.io>
2024-03-19 10:57:55 +0100

    [ADD] method mine_in_matrice / test : ok

--- Grid.py
+++ Grid.py
@@ -52,17 +52,40 @@ class Grid:
             for j in range(total_y):
                 self.__matrice[i].append(0)
 
+    # Define randomly the number of mines depending on the level
     def mine_number(self):
         if self.__level == 'easy':
-            return 10
+            min_mine = 5
+            max_mine = 11
+            random_mine = random.randint(min_mine, max_mine)
         elif self.__level == 'medium':
-            return 40
+            min_mine = 20
+            max_mine = 41
+            random_mine = random.randint(min_mine, max_mine)
         elif self.__level == 'hard':
-            return 99
+            min_mine = 50
+            max_mine = 99
+            random_mine = random.randint(min_mine, max_mine)
+        return random_mine
+    
+    # Place the mines in the matrice with the value -1
+    def mine_in_matrice(self):
+        mine_number = self.mine_number()
+        total_x, total_y = self.matrice_size()
+        for i in range(mine_number):
+            x = random.randint(0, total_x-1)
+            y = random.randint(0, total_y-1)
+            if self.__matrice[x][y] != -1:
+                self.__matrice[x][y] = -1
+            else:
+                i -= 1
+
+
 
 
 # grid = Grid('easy')
 # grid.fill_matrice()
+# grid.mine_in_matrice()
 # print(grid.get_matrice())
     
     
