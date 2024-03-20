a8e41939b415d7a1d4f39c85b036d685b5820c85
Dhjali <barbara.vanni@laplateforme.io>
2024-03-20 10:34:24 +0100

    [ADD] method win and lose

--- StateGame.py
+++ StateGame.py
@@ -1,8 +1,10 @@
 from Grid import Grid
+from MouseClick import MouseClick
 
 class StateGame:
     def __init__(self, level):
         self.__grid_object = Grid(level)
+        self.__mouse_click = MouseClick()
 
     def get_grid_object(self):
         return self.__grid_object
@@ -12,10 +14,17 @@ class StateGame:
 
 
     def lose(self):
-       return self.__grid_object.lose()
+        x, y = self.__mouse_click.left_click()
+        if self.__grid_object.get_matrice()[x][y] == -1:
+            return True
+
 
     def win(self):
+        x, y = self.__mouse_click.left_click()
         for cell in self.__grid_object.list_cells_objects:
-            if cell == -1 and cell.get_state() == False:
+            if cell.get_state() == False and self.__grid_object.get_matrice()[x][y] == -1:
                 return True
-            pass
+            
+    
+
+    
