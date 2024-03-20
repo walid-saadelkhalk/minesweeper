4935565362762402d917fc7073d1f5ffc260298f
Dhjali <barbara.vanni@laplateforme.io>
2024-03-20 12:00:35 +0100

    [ADD] method next_move

--- StateGame.py
+++ StateGame.py
@@ -28,7 +28,46 @@ class StateGame:
     def initialize_game(self):
         self.__grid_object.set_matrice([])
         self.__grid_object.filled_matrice()
+        self.__grid_object.set_list_cells_objects([])
         self.__grid_object.fill_list_cells_objects()
+
+    def next_move(self):
+        x, y = self.__mouse_click.left_click()
+        total_x, total_y = self.__grid_object.matrice_size()
+        list_looking_for_zero = []
+        if self.__grid_object.get_matrice()[x][y] == 0:
+            for i in range(x-1, x+2):
+                for j in range(y-1, y+2):
+                    if i >= 0 and i <= total_x and j >= 0 and j <= total_y:
+                        if self.__grid_object.get_matrice()[i][j] != -1:
+                            for cell in self.__grid_object.list_cells_objects:
+                                if cell.get_position() == (i,j):
+                                    cell.set_state(True)
+                                    if cell.get_position() != (x,y):
+                                        list_looking_for_zero.append(cell)
+        for zero in list_looking_for_zero:
+            if zero == 0 : 
+                self.next_move()
+
+                                    
+
+
+            # if i > 0 and j > 0 and self.__matrice[i-1][j-1] != -1:
+            #     self.__matrice[i-1][j-1] += 1
+            # if i > 0 and self.__matrice[i-1][j] != -1:
+            #     self.__matrice[i-1][j] += 1
+            # if i > 0 and j < total_y-1 and self.__matrice[i-1][j+1] != -1:
+            #     self.__matrice[i-1][j+1] += 1
+            # if j > 0 and self.__matrice[i][j-1] != -1:
+            #     self.__matrice[i][j-1] += 1
+            # if j < total_y-1 and self.__matrice[i][j+1] != -1:
+            #     self.__matrice[i][j+1] += 1
+            # if i < total_x-1 and j > 0 and self.__matrice[i+1][j-1] != -1:
+            #     self.__matrice[i+1][j-1] += 1
+            # if i < total_x-1 and self.__matrice[i+1][j] != -1:
+            #     self.__matrice[i+1][j] += 1
+            # if i < total_x-1 and j < total_y-1 and self.__matrice[i+1][j+1] != -1:
+            #     self.__matrice[i+1][j+1] += 1 
     
     
     
