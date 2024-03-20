66ece8ca5b5b88b9bd57e46560473608619dff52
Sluchai40 <mabbolos8@gmail.com>
2024-03-20 09:59:22 +0100

    [MODIFY] clean the Button class to use only the draw part for the moment

--- Button.py
+++ Button.py
@@ -1,21 +1,19 @@
-import pygame
-import time
-
-
-
 class Button():
     def __init__(self, x, y, image, hover_image=None):
-        self.image = image
-        self.hover_image = hover_image
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (x, y)
-        self.clicked = False
-        self.active = True
-        self.last_click_time = time.time()
+        self.__image = image
+        self.__hover_image = hover_image
+        self.__rect = self.__image.get_rect()
+        self.__rect.topleft = (x, y)
+        # self.__clicked = False
+        # self.__active = True
+        # self.__last_click_time = time.time()
+    
+    def get_rect(self):
+        return self.__rect
 
-
+    # Methode that draw button
     def draw(self, surface):
-        surface.blit(self.image, self.rect)
+        surface.blit(self.__image, self.__rect)
 
 
     # def check_clicked(self):
@@ -32,9 +30,9 @@ class Button():
     #         self.clicked = False
     #     return action
 
-    def render(self, screen):
-        if self.active:
-            self.draw(screen)
-            return self.check_clicked()
-        else:
-            return False
+    # def render(self, screen):
+    #     if self.__active:
+    #         self.draw(screen)
+            # return self.check_clicked()
+        # else:
+        #     return False
