e9cafab0adaa8276c395bc4a63522b3b852f6758
walid-saadelkhalk <walid.saad-el-khalid@laplateforme.io>
2024-03-20 09:24:37 +0100

    [ADD] modification i timer function, now we can pause the time

--- time.py
+++ time.py
@@ -1,11 +1,21 @@
 import sys
 import time
+import keyboard 
 
-def decompte():
+def timer():
     for i in range(1000):
         sys.stdout.write( str(i).zfill(3) + '\r')
         sys.stdout.flush()
+        if keyboard.is_pressed('space'):
+            input('Paused, press enter to continue')
         time.sleep(1)
-decompte()
+timer()
 
 
+  
+
+
+
+
+
+ 
