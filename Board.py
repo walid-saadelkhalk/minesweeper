d16b27f1bc79b05d7e8b74d3132bfe4b99aba442
Dhjali <barbara.vanni@laplateforme.io>
2024-03-21 22:26:31 +0100

    [ADD] assets and create button in Board.py

--- Board.py
+++ Board.py
@@ -72,7 +72,7 @@ class Board:
 
             self.__screen.blit(grid_surface, (0, self.__visual_info_height))
         else:
-            print("The screen is not defined")
+            print("The screen is not defined 0")
 
     # Return the color of the figure depending on the number
     def color_figures(self, number):
@@ -95,7 +95,7 @@ class Board:
                             text = font.render(str(value), 1, color)
                             self.__screen.blit(text, (i * self.__cell_size + 9, j * self.__cell_size + 5 + self.__visual_info_height))
         else:
-            print("The screen is not defined")
+            print("The screen is not defined 1")
 
     # Draw the mines in the matrice
     def draw_mines(self):
@@ -108,7 +108,7 @@ class Board:
             for mines in self.__mines_list:
                 mines.draw_image(self.__screen)
         else:
-            print("The screen is not defined")
+            print("The screen is not defined 2")
 
     def button_cell(self):
         if self.__screen is not None:
@@ -123,7 +123,7 @@ class Board:
         for button in self.__button_list:
             button.draw(self.__screen)
         else:
-            print("The screen is not defined")
+            print("The screen is not defined 3")
 
     def load_board(self):
         # Load game board
@@ -136,4 +136,9 @@ class Board:
 
         # Load visual info bar
         self.visual_info = VisualInfo(self.__screen, self.__visual_info_height)
+        self.visual_info.add_button(0, 8, Image("./assets/back.png", (0, 0)).get_image_surface())
+        self.visual_info.add_button((self.__screen.get_width() // 2  - 15), 8, Image("./assets/smiley_ok.png", (0, 0)).get_image_surface())
+        self.visual_info.add_button(self.__screen.get_width() - self.__cell_size, 8, Image("./assets/question_mark.png", (0, 0)).get_image_surface())
+
         self.visual_info.draw(self.__screen)
+
--- VisualInfo.py
+++ VisualInfo.py
@@ -1,4 +1,5 @@
 import pygame
+from Button import Button
 
 '''
 class 
@@ -9,27 +10,32 @@ class VisualInfo:
         self.screen_width = screen_width
         self.visual_info_height = visual_info_height
         self.background_color = (192, 192, 192)
-        self.button_color = (0, 128, 0)
-        self.font = pygame.font.Font(None, 36)
+        self.button_color = (0, 0, 0)
+        self.font = pygame.font.Font(None, 20)
         self.buttons = []
 
-    def add_button(self, text, pos):
-        button_rect = pygame.Rect(pos[0], pos[1], 150, 50)
-        self.buttons.append((text, button_rect))
+    def add_button(self, x, y, image, hover_image=None):
+        button = Button( x, y, image, hover_image=None)
+        # button_rect = pygame.Rect(pos[0], pos[1], 150, 50)
+        self.buttons.append((button))
+
+    def timer(self, time):
+        text_surface = self.font.render(str(time), True, (255, 255, 255))
+        text_rect = text_surface.get_rect(center=(self.screen_width.get_width() - 50, self.visual_info_height // 2))
+        self.screen.blit(text_surface, text_rect)
 
     def draw(self, screen):
-        # # Draw background
-        print("Drawing visual info")
+        # Draw background
         screen_width = self.screen_width.get_width()
-        print(screen_width)
-        print(self.visual_info_height)
         pygame.draw.rect(screen, self.background_color, (0, 0, screen_width, self.visual_info_height))
 
         # Draw buttons
-        for text, rect in self.buttons:
-            pygame.draw.rect(screen, self.button_color, rect)
-            text_surface = self.font.render(text, True, (255, 255, 255))
-            text_rect = text_surface.get_rect(center=rect.center)
-            screen.blit(text_surface, text_rect)
+        for button in self.buttons:
+            button.draw(screen) 
+        # for text, rect in self.buttons:
+        #     pygame.draw.rect(screen, self.button_color, rect)
+        #     text_surface = self.font.render(text, True, (255, 255, 255))
+        #     text_rect = text_surface.get_rect(center=rect.center)
+        #     screen.blit(text_surface, text_rect)
 
         
