93e1b05f1c6862cc291f23c8a8aced905a447bc4
Dhjali <barbara.vanni@laplateforme.io>
2024-03-21 22:39:04 +0100

    [ADD] method to draw timer and bomb_count

--- VisualInfo.py
+++ VisualInfo.py
@@ -2,7 +2,9 @@ import pygame
 from Button import Button
 
 '''
-class 
+class visual info for the render of the head bar of the game
+we can see the number of bombs and the timer
+
 '''
 
 class VisualInfo:
@@ -13,17 +15,23 @@ class VisualInfo:
         self.button_color = (0, 0, 0)
         self.font = pygame.font.Font(None, 20)
         self.buttons = []
+        self.bomb_counter = 0
+        self.timer_value = 0
 
+
+    #method that add a button to the visual info bar
     def add_button(self, x, y, image, hover_image=None):
         button = Button( x, y, image, hover_image=None)
         # button_rect = pygame.Rect(pos[0], pos[1], 150, 50)
         self.buttons.append((button))
 
-    def timer(self, time):
-        text_surface = self.font.render(str(time), True, (255, 255, 255))
-        text_rect = text_surface.get_rect(center=(self.screen_width.get_width() - 50, self.visual_info_height // 2))
-        self.screen.blit(text_surface, text_rect)
+    def set_bomb_counter(self, count):
+        self.bomb_counter = count
 
+    def set_timer_value(self, value):
+        self.timer_value = value
+
+    #method that draw the visual info bar
     def draw(self, screen):
         # Draw background
         screen_width = self.screen_width.get_width()
@@ -32,10 +40,17 @@ class VisualInfo:
         # Draw buttons
         for button in self.buttons:
             button.draw(screen) 
-        # for text, rect in self.buttons:
-        #     pygame.draw.rect(screen, self.button_color, rect)
-        #     text_surface = self.font.render(text, True, (255, 255, 255))
-        #     text_rect = text_surface.get_rect(center=rect.center)
-        #     screen.blit(text_surface, text_rect)
+
+        # Draw bomb counter
+        bomb_text = f"{self.bomb_counter}"
+        bomb_surface = self.font.render(bomb_text, True, (0, 0, 0), (255, 255, 255))
+        bomb_rect = bomb_surface.get_rect(midleft=(60, self.visual_info_height // 2))
+        screen.blit(bomb_surface, bomb_rect)
+
+        # Draw timer
+        timer_text = f"{self.timer_value}"
+        timer_surface = self.font.render(timer_text, True, (0, 0, 0), (192, 192, 192))
+        timer_rect = timer_surface.get_rect(midright=(self.screen_width.get_width() - 60 , self.visual_info_height // 2))
+        screen.blit(timer_surface, timer_rect)
 
         
